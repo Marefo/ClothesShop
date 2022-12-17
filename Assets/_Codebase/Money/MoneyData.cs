616be580012ab16c1f2e041b955c1ffad8d0af06
Marefo <79011582+Marefo@users.noreply.github.com>
2022-12-17 13:32:58 +0200

    added money

---
+++ Assets/_Codebase/Money/MoneyData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace _Codebase.Money
+{
+  [CreateAssetMenu(fileName = "MoneyData", menuName = "StaticData/Money")]
+  public class MoneyData : ScriptableObject
+  {
+    public event Action<int> AmountChanged;
+
+    public int Amount;
+    public int StartAmount;
+
+    public void Add(int amount)
+    {
+      Amount += amount;
+      AmountChanged?.Invoke(Amount);
+    }
+
+    public int Remove(int amount)
+    {
+      int shortage = Amount - amount;
+			
+      Amount = Mathf.Clamp(Amount - amount, 0, int.MaxValue);
+      AmountChanged?.Invoke(Amount);
+
+      return shortage < 0 ? Mathf.Abs(shortage) : 0;
+    }
+
+    [Button()]
+    public void Reset()
+    {
+      Amount = StartAmount;
+      AmountChanged?.Invoke(Amount);
+    }
+  }
+}
---
+++ Assets/_Codebase/UI/MoneyAmountVisualizer.cs
@@ -0,0 +1,18 @@
+﻿using _Codebase.Money;
+using TMPro;
+using UnityEngine;
+
+namespace _Codebase.UI
+{
+  public class MoneyAmountVisualizer : MonoBehaviour
+  {
+    [SerializeField] private TextMeshProUGUI _field;
+    [SerializeField] private MoneyData _moneyData;
+
+    private void OnEnable() => _moneyData.AmountChanged += SetFieldValue;
+    private void OnDisable() => _moneyData.AmountChanged -= SetFieldValue;
+
+    private void Start() => SetFieldValue(_moneyData.Amount);
+    private void SetFieldValue(int value) => _field.text = value.ToString();
+  }
+}
